109e1914306913620abb3314f9a02245daba0e16
Bapponi <aleksandarbubalo99@gmail.com>
2024-12-14 15:49:26 +0100

    add second problem to stack package (renamed from threes)

---
+++ src/main/java/stack/DailyTemperatures739.java
@@ -0,0 +1,20 @@
+package stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class DailyTemperatures739 {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        DailyTemperatures739 solution = new DailyTemperatures739();
+
+        int [] temperatures = {30,38,30,36,35,40,28};
+
+        int [] result = solution.dailyTemperatures(temperatures);
+        System.out.println("Result: " + result);
+    }
+}
